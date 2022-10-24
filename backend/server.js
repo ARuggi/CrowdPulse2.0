f1b4651997ea6e54de59281dbdb0f5bf6d7cb7cc
ARuggi <a.ruggieri31@studenti.uniba.it>
2022-10-24 19:48:20 +0200

    Renamed routes.js to tweetRoutes.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const tweetRoute = require("./routes/routes");
+const tweetRoute = require("./routes/tweetRoutes");
 
 const app = express();
 app.use(express.json());
