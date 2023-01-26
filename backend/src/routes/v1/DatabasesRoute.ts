e2059dc94612ce3522d47a1c75732e3ce762671e
ARuggi <a.ruggieri31@studenti.uniba.it>
2023-01-26 17:26:10 +0100

    Added databases filters to endpoint GET - /v1/databases

--- backend/src/routes/v1/DatabasesRoute.ts
+++ backend/src/routes/v1/DatabasesRoute.ts
@@ -4,58 +4,53 @@ import {asyncFilter} from '../../util/AsyncUtil';
 import {createResponse, ResponseType} from '../IRoute';
 import {getAdminConnection} from '../../database/database';
 import {
-    CrowdPulseDatabaseInfo,
     getDatabaseCollectionsInfo,
     getCrowdPulseDatabaseInfo,
     isCrowdPulseCollection,
     isReservedDatabase} from '../../util/DatabaseUtil';
 
-type ResultType = {
-    databases: Array<{
-        name: string,       // Database name
-        sizeOnDisk: number, // Size in Kbps
-        empty: boolean      // True if it is an empty database
-        info: CrowdPulseDatabaseInfo // database information
-    }>
-}
-
 // noinspection DuplicatedCode
 export class DatabasesRoute extends AbstractRoute {
 
     async performRequest(req: Request, res: Response): Promise<void> {
+        let filters = {dbs: []};
+
+        if (req?.query?.dbs) {
+            filters.dbs.push(req.query.dbs);
+        }
+
         try {
-            getAdminConnection()
-                .db
-                .admin()
-                .listDatabases(async (error, result: ResultType) => {
 
-                    if (error) {
-                        throw error;
-                    }
-
-                    result.databases = await asyncFilter(result.databases, async (database) => {
-                        if (isReservedDatabase(database.name)) return false;
-
-                        return await getDatabaseCollectionsInfo(database.name)
-                            .then((result) => {
-                                return !result
-                                    .filter(r => {return isCrowdPulseCollection(r)})
-                                    .empty;
-                            });
-                    });
-
-                    for (const database of result.databases) {
-                        database.info = await getCrowdPulseDatabaseInfo(database.name);
-                    }
-
-                    res.send(createResponse(ResponseType.OK, undefined, result));
-                });
+            let result = await this.getInfoFromDatabase(filters);
+            res.send(createResponse(ResponseType.OK, undefined, result));
 
         } catch (error) {
-            console.error(error);
+            console.log(error)
             res.status(500);
             res.send(createResponse(ResponseType.KO, error.message));
         }
+    }
+
+    private async getInfoFromDatabase(filters: {dbs: Array<string>}): Promise<any> {
+        let admin = getAdminConnection().db.admin();
+        let result = await admin.listDatabases();
+
+        result.databases = await asyncFilter(result.databases, async (database) => {
+            if (isReservedDatabase(database.name)
+                || (filters.dbs.length !== 0
+                    && !filters.dbs.find(name => name == database.name))) {
+                return false;
+            }
+
+            let collectionsInfo = await getDatabaseCollectionsInfo(database.name);
+            return !collectionsInfo.filter(r => isCrowdPulseCollection(r)).empty;
+        });
+
+        for (const database of result.databases) {
+            database.info = await getCrowdPulseDatabaseInfo(database.name);
+        }
+
+        return result;
     }
 
     protected path(): string {
--- backend/src/util/AsyncUtil.ts
+++ backend/src/util/AsyncUtil.ts
@@ -8,8 +8,9 @@
  *
  * @param array The array.
  * @param predicate The filtering predicate.
+ * @return A promise with the filtered array values.
  */
-export async function asyncFilter(array, predicate) {
+export async function asyncFilter(array, predicate): Promise<any> {
     const results = await Promise.all(array.map(predicate));
     return array.filter((_v, index) => results[index]);
 }
