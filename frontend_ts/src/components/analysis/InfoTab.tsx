579c00bd9ceed5d39ce54deb2ddd44a8ba214689
ARuggi <a.ruggieri31@studenti.uniba.it>
2023-02-12 14:13:50 +0100

    Improved InfoTab

--- frontend_ts/src/components/analysis/InfoTab.tsx
+++ frontend_ts/src/components/analysis/InfoTab.tsx
@@ -2,15 +2,34 @@ import React, {useEffect, useState} from 'react';
 import api from '../../api';
 import {DatabasesResponse} from '../../api/DatabasesResponse';
 import {useTranslation} from 'react-i18next';
-import {Center, Loader} from '@mantine/core';
+import {
+    Center,
+    Container,
+    Divider, Flex,
+    Highlight,
+    Loader,
+    Stack,
+    Table,
+    Title,
+    TypographyStylesProvider
+} from '@mantine/core';
+import {TbFileDatabase} from "react-icons/tb";
 
 interface IProps {
     dbs: string[]
 }
 
+const bytesToKiloBytes = (bytes: number) => {
+    return bytes / 1024;
+}
+
+const kiloBytesToMegaBytes = (kiloBytes: number) => {
+    return kiloBytes / 1024;
+}
+
 const InfoTab:React.FC<IProps> = ({dbs}) => {
 
-    const {t} = useTranslation();
+    const { t } = useTranslation();
     const [isError, setError] = useState(false);
     const [databasesData, setDatabasesData] = useState<DatabasesResponse | null>(null);
 
@@ -34,7 +53,68 @@ const InfoTab:React.FC<IProps> = ({dbs}) => {
         return <Center><Loader size='xl' variant='dots' /></Center>
     }
 
-    return <p>{JSON.stringify(databasesData)}</p>
+    return <Stack>{
+        databasesData.databases.map(database => {
+            return <div key={database.name}>
+                <Container>
+                    <Flex
+                        gap="md"
+                        justify="left"
+                        align="center"
+                        direction="row"
+                        wrap="wrap">
+                        <TbFileDatabase size={30} style={{
+                            marginTop: '5px',
+                            color: '#38a0df',
+                        }}/>
+                        <Title order={1}>
+                            <Highlight
+                                align="center"
+                                highlight={database.name}
+                                highlightStyles={(theme) => ({
+                                    backgroundImage: theme.fn.linearGradient(45, theme.colors.cyan[5], theme.colors.indigo[5]),
+                                    fontWeight: 700,
+                                    WebkitBackgroundClip: 'text',
+                                    WebkitTextFillColor: 'transparent',
+                                })}>
+                                {database.name}
+                            </Highlight>
+                        </Title>
+                    </Flex>
+                    <Table
+                        style={{marginTop: '20px', marginBottom: '20px'}}
+                        striped
+                        withBorder
+                        withColumnBorders
+                        verticalSpacing='xs'
+                        fontSize='md'>
+                        <thead>
+                        <tr>
+                            <th>{t('releaseDate')}</th>
+                            <th>{t('lastUpdate')}</th>
+                            <th>{t('version')}</th>
+                            <th>{t('sizeOnDisk')}</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>{database.info.releaseDate ? database.info.releaseDate.toString() : t('notDefined')}</td>
+                            <td>{database.info.lastUpdateDate ? database.info.lastUpdateDate.toString() : t('notDefined')}</td>
+                            <td>{database.info.version ? database.info.version : t('notDefined')}</td>
+                            <td>{kiloBytesToMegaBytes(bytesToKiloBytes(database.sizeOnDisk)).toFixed(2)} MB</td>
+                        </tr>
+                        </tbody>
+                    </Table>
+                    <Title order={2}>{t('description')}</Title>
+                    <TypographyStylesProvider>
+                        {database?.info?.htmlDescription && <div dangerouslySetInnerHTML={{__html: database.info.htmlDescription}}/>}
+                        {!database?.info?.htmlDescription && <p>{t('noDescription')}</p>}
+                    </TypographyStylesProvider>
+                    <Divider/>
+                </Container>
+            </div>
+        })
+    }</Stack>
 }
 
 export default InfoTab;
--- frontend_ts/src/components/analysis/index.tsx
+++ frontend_ts/src/components/analysis/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import {useSearchParams} from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import {useLocation, useSearchParams} from 'react-router-dom';
+import {useTranslation} from "react-i18next";
 import {Tabs} from '@mantine/core';
 
 import {AiFillInfoCircle, AiFillFileWord, AiOutlineUnorderedList} from 'react-icons/ai';
 import {MdSentimentSatisfiedAlt} from 'react-icons/md';
 import {RiTimeLine} from 'react-icons/ri';
 import {BsMap} from 'react-icons/bs';
+import {FiSettings} from "react-icons/fi";
 
 import InfoTab from './InfoTab';
 import SentimentTab from './SentimentTab';
@@ -14,21 +16,30 @@ import TimelineTab from './TimelineTab';
 import TweetListTab from './TweetListTab';
 import MapTab from './MapTab';
 
-const Analysis = () => {
 
+const Analysis = () => {
+    const { t } = useTranslation();
+    const location = useLocation();
     const [query] = useSearchParams();
+    const [reload, setReload] = useState(false);
+
+    useEffect(() => {
+        if (!reload) setReload(true);
+        else window.location.reload();
+    }, [location.search]);
+
     const dbs: string[] | undefined = query.getAll('dbs');
 
-    return <Tabs variant='outline' defaultValue='info'>
+    return <Tabs variant='pills' defaultValue='info'>
         <Tabs.List>
-            <Tabs.Tab value='info' icon={<AiFillInfoCircle size={14} />}>info</Tabs.Tab>
-            <Tabs.Tab value='sentiment' icon={<MdSentimentSatisfiedAlt size={14} />}>Sentiment</Tabs.Tab>
-            <Tabs.Tab value='word' icon={<AiFillFileWord size={14} />}>Word</Tabs.Tab>
-            <Tabs.Tab value='timeline' icon={<RiTimeLine size={14} />}>Timeline</Tabs.Tab>
-            <Tabs.Tab value='tweet_list' icon={<AiOutlineUnorderedList size={14} />}>Tweet list</Tabs.Tab>
-            <Tabs.Tab value='map' icon={<BsMap size={14} />}>Map</Tabs.Tab>
+            <Tabs.Tab value='info' icon={<AiFillInfoCircle size={14} />}>{t('info')}</Tabs.Tab>
+            <Tabs.Tab value='sentiment' icon={<MdSentimentSatisfiedAlt size={14} />}>{t('sentiment')}</Tabs.Tab>
+            <Tabs.Tab value='word' icon={<AiFillFileWord size={14} />}>{t('word')}</Tabs.Tab>
+            <Tabs.Tab value='timeline' icon={<RiTimeLine size={14} />}>{t('timeline')}</Tabs.Tab>
+            <Tabs.Tab value='tweet_list' icon={<AiOutlineUnorderedList size={14} />}>{t('tweetList')}</Tabs.Tab>
+            <Tabs.Tab value='map' icon={<BsMap size={14} />}>{t('map')}</Tabs.Tab>
+            <Tabs.Tab value='settings' icon={<FiSettings size={14} />} ml="auto">{t('settings')}</Tabs.Tab>
         </Tabs.List>
-
         <Tabs.Panel value='info' pt='xs'><InfoTab dbs={dbs}/></Tabs.Panel>
         <Tabs.Panel value='sentiment' pt='xs'><SentimentTab dbs={dbs}/></Tabs.Panel>
         <Tabs.Panel value='word' pt='xs'><WordTab dbs={dbs}/></Tabs.Panel>
